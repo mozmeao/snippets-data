cc3fc4ea8dd78de3f6680927292996900138faf0
Josh Mize <jgmize@gmail.com>
2020-01-06 15:08:49 -0600

    Fix quarter directory

--- k8s-nbconvert.sh
+++ k8s-nbconvert.sh
@@ -19,13 +19,13 @@ if [[ "$2" =~ "commit" ]]; then
         NOTEBOOK_PATH="$YM/$NOTEBOOK"
     elif [[ $2 =~ quarter ]]; then
         MONTH=$(date --date="1 month ago" +%m)
-        if $MONTH in 1 2 3; then
+        if (( $MONTH <= 3 )); then
             Q=Q1
-        elif $MONTH in 4 5 6; then
+        elif (( $MONTH <= 6 )); then
             Q=Q2
-        elif $MONTH in 7 8 9; then
+        elif (( $MONTH <= 9 )); then
             Q=Q3
-        elif $MONTH in 10 11 12; then
+        elif (($MONTH <= 12 )); then
             Q=Q4
         fi
         YQ=$(date --date="1 month ago" +%Y)/$Q
